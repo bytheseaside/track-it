51b2ad148e2cf35242b6d04e32971a971019cb68
Brisa Rojas <rojaasbrisa@gmail.com>
2023-11-19 13:17:48 -0300

    refactor: refactor page to match layout changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 
-import ShouldTakeBreak from './ShouldTakeBreak';
 import ToDoBoard from './ToDoBoard';
 
 export default function Home() {
   return (
-    <>
-      <ToDoBoard />
-      <ShouldTakeBreak />
-    </ >
+    <ToDoBoard />
   );
 }
